22188ebaff363ac6011e79074a0ff05a8311df93
joseph.yoon <joseph.yoon@veritas.com>
2017-02-01 17:56:31 -0800

    chapter 2

--- base/base.tf
+++ base/base.tf
@@ -1,7 +1,11 @@
-provider "aws" { }
+provider "aws" {}
 
 # Create a web server
 resource "aws_instance" "base" {
-ami           = "ami-76227116"
-instance_type = "t2.micro"
+  ami           = "ami-76227116"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "base" {
+  instance = "${aws_instance.base.id}"
 }
